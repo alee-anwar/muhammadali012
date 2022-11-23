8845477ebd9b5c2958a58c48372f2a57d0b2510c
alee-anwar <fa19-bse-012@cuilahore.edu.pk>
2022-11-23 07:12:50 +0500

    Create Bookmark.js

---
+++ Screen/Bookmark.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Bookmark() {
+  return (
+    <View style={styles.container}>
+    <Text>Bookmark is empty</Text>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EFF5F5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

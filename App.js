c87a1f0f43e61898188cabc4ba4dd678fd817404
alee-anwar <fa19-bse-012@cuilahore.edu.pk>
2022-11-23 07:12:14 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+// import Home from './Screen/Home';
+import ShopNow from './Screen/ShopNow';
+import Bookmark from './Screen/Bookmark';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleAlign: 'center'
+      }}>
+      {/* <Stack.Screen name="Home" component={Home} options={{ title: 'Welcome' }}/> */}
+      <Stack.Screen name="ShopNow" component={ShopNow} options={{ title: 'Shop Now'}}/>
+      <Stack.Screen name="Bookmark" component={Bookmark}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
